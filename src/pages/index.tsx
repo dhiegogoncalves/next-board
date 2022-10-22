52c7caab3f88d2abb986054737a011f3107c7b50
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2022-10-22 14:49:26 -0400

    add global style.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import Head from 'next/head';
 import styles from '../styles/styles.module.scss';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <title>Meu Board - Organizando suas tarefas</title>
+      </Head>
+      <div></div>
+    </>
+  );
 }
